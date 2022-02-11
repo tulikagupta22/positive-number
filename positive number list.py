c29a68c1c664f22e9af11630452368f61a3ecf9c
tulikagupta22 <76577676+tulikagupta22@users.noreply.github.com>
2022-02-11 23:13:13 +0530

    Create positive number list.py

---
+++ positive number list.py
@@ -0,0 +1,17 @@
+list1 = [12, -7, 5, 64, -14]
+
+# iterating each number in list
+for num in list1:
+
+    # checking condition
+    if num >= 0:
+        print(num, end=" ")
+
+print("\n")
+
+list1 = [12, 14, -95,  3]
+
+for num in list1:
+
+    if num >= 0:
+        print(num, end=" ")
